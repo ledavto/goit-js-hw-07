f636971e76351dc10f0c81655a1b0a5694b15ce2
ledavto <ser_ega@ukr.net>
2023-09-07 21:56:17 +0300

    edit HW

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -40,27 +40,29 @@ ulEl.insertAdjacentHTML("beforeend", imgEl);
 // });
 
 ulEl.addEventListener("click", (event) => {
+  event.preventDefault();
   if (event.target.nodeName !== "IMG") {
     return;
   }
-    
-    event.preventDefault();
-  console.log("event.target: ", event.target);
-  console.log("event.currentTarget: ", event.currentTarget);
+  
+  // console.log("event.target: ", event.target);
+  // console.log("event.currentTarget: ", event.currentTarget);
+
+ // import * as basicLightbox from 'basiclightbox'
+ 
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600" 
+    >
+`)
+  instance.show();
+
+  // document.addEventListener("keydown", event => {
+  //   if (event.code === "Escape") 
+  // console.log("key: ", event.key);
+  // console.log("code: ", event.code);
+//});
+  
 });
 
 
-// import * as basicLightbox from 'basiclightbox'
-
-// const instance = basicLightbox.create(`
-//     <div class="modal">
-//         <p>
-//             Your first lightbox with just a few lines of code.
-//             Yes, it's really that simple.
-//         </p>
-//     </div>
-// `)
-
-// instance.show();
-
-console.log(galleryItems);
+//console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const ulEl = document.querySelector(".gallery");
+
+const imgEl = galleryItems.map(({preview,original,description }) => 
+    `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" data-caption="${description}" alt="${description}" />
+   </a>
+</li>`
+).join("");
+
+ulEl.insertAdjacentHTML("beforeend", imgEl);
+
+ulEl.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  
+  // console.log("event.target: ", event.target);
+  // console.log("event.currentTarget: ", event.currentTarget);
+
+ // import * as basicLightbox from 'basiclightbox'
+    //new SimpleLightbox('.some-element a', { /* options */ });
+  console.log(event.target.dataset.caption);
+  let gallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250
+     });
+// gallery.on('show.simplelightbox', function () {
+// 	// do something…
+// });
+
+  // document.addEventListener("keydown", event => {
+  //   if (event.code === "Escape") 
+  // console.log("key: ", event.key);
+  // console.log("code: ", event.code);
+//});
+  
+});
