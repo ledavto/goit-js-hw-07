fdbe2cb39f8f22d457f3d196b5dc6a1d8de9f4df
ledavto <ser_ega@ukr.net>
2023-09-08 12:23:37 +0300

    Edit after mentor

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,28 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const ulEl = document.querySelector(".gallery");
 
-const imgEl = galleryItems.map(({preview,original,description }) => 
-    `<li class="gallery__item">
+const imgEl = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
    <a class="gallery__link" href="${original}">
       <img class="gallery__image" src="${preview}" data-caption="${description}" alt="${description}" />
    </a>
 </li>`
-).join("");
+  )
+  .join("");
 
 ulEl.insertAdjacentHTML("beforeend", imgEl);
 
-ulEl.addEventListener("click", (event) => {
-  event.preventDefault();
-  if (event.target.nodeName !== "IMG") {
-    return;
-  }
-  
-
-  let gallery = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionDelay: 250
-     });
-  
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
 });
